c248719f1faee7c3b7d438dbadc0d86a8de89db6
Alejandro Machado <alemacgo@gmail.com>
2012-11-08 14:36:12 -0430

    Reorganize dotfiles

--- get.bash
+++ get.bash
@@ -1,28 +1,38 @@
 #!/bin/bash
-cp /Users/alejandro/.vimrc .
-cp /Users/alejandro/.gvimrc .
-cp /Users/alejandro/.pentadactylrc .
-cp /Users/alejandro/.gitconfig .
-cp /Users/alejandro/.gitignore .
+mkdir -p vim
+cp /Users/alejandro/.vimrc vim/
+cp /Users/alejandro/.gvimrc vim/
+mkdir -p vim/.vim
+cp -r /Users/alejandro/.vim/autoload vim/.vim/
+cp -r /Users/alejandro/.vim/bundle vim/.vim/
+cp -r /Users/alejandro/.vim/colors vim/.vim/
+cp -r /Users/alejandro/.vim/compiler vim/.vim/
+cp -r /Users/alejandro/.vim/doc vim/.vim/
+cp -r /Users/alejandro/.vim/ftplugin vim/.vim/
+cp -r /Users/alejandro/.vim/plugin vim/.vim/
+cp -r /Users/alejandro/.vim/syntax vim/.vim/
 
-brew list > homebrew.list
+mkdir -p firefox
+cp /Users/alejandro/.pentadactylrc firefox/
 
-mkdir -p .ssh
-cp /Users/alejandro/.ssh/id_dsa.pub .ssh
+mkdir -p git
+cp /Users/alejandro/.gitconfig git/
+cp /Users/alejandro/.gitignore git/
 
-mkdir -p .config/fish/
-cp /Users/alejandro/.config/fish/config.fish .config/fish/
+mkdir -p brew
+brew list > brew/homebrew.list
 
-mkdir -p .vim
-cp /Users/alejandro/.vim/filetype.vim .vim/
-cp -r /Users/alejandro/.vim/autoload .vim/
-cp -r /Users/alejandro/.vim/bundle .vim/
-cp -r /Users/alejandro/.vim/colors .vim/
-cp -r /Users/alejandro/.vim/compiler .vim/
-cp -r /Users/alejandro/.vim/doc .vim/
-cp -r /Users/alejandro/.vim/ftplugin .vim/
-cp -r /Users/alejandro/.vim/plugin .vim/
-cp -r /Users/alejandro/.vim/syntax .vim/
+mkdir -p ssh/.ssh
+cp /Users/alejandro/.ssh/id_dsa.pub ssh/.ssh/
+
+mkdir -p fish/.config/fish/
+cp /Users/alejandro/.config/fish/config.fish fish/.config/fish/
 
 mkdir -p usr/local/bin
 cp /usr/local/bin/mvim usr/local/bin
+
+mkdir -p 'keyremap/Library/Application Support/KeyRemap4MacBook'
+cp '/Users/alejandro/Library/Application Support/KeyRemap4MacBook/private.xml' 'keyremap/Library/Application Support/KeyRemap4MacBook'
+
+mkdir -p 'quicksilver/Library/Application Support/Quicksilver'
+cp '/Users/alejandro/Library/Application Support/Quicksilver/triggers.plist' 'quicksilver/Library/Application Support/Quicksilver'
